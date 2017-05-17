33937fada7db3a7cd0b42faac5b83f34fbfc47c5
Eunice Wanjiru <eunice.wanjiru@andela.com>
2017-05-17 11:06:48 +0300

    [header component] add a header with menu and title

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,20 +1,22 @@
 import React from "react";
-import './header.scss';
+import './Header.scss';
 export default class Header extends React.Component {
-    render() {
-        return (
-            <div className="header">
-                <div className="logo">
-                    <img src='./images/globe1.png' alt="earth" />
-                </div>
-                <span className="title">{this.props.title}</span>
-                <div className="searchSource">
-                    <input type="text" className="searchField" placeholder="search a source" />
-                    <div className="searchIcon">
-                        <img src="./images/icon.png" alt="search" />
-                    </div>
-                </div>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="header">
+				<div className="menuIcon">
+				<img src = "./../../../images/menu.png" alt= "icon"/>
+				</div>
+
+				<span className="title">{this.props.title}</span>
+
+                <input type="text" className="searchField" placeholder="search a source" />
+				
+				<div className="searchIcon" >
+					
+				</div>
+
+			</div>
+		);
+	}
 }
