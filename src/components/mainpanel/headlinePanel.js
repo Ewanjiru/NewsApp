4ea6505640f13c1fcd373def9e6e8475c98784f9
Ewanjiru <eunice.wanjiru@andela.com>
2017-05-22 11:45:36 +0300

    Revert "[flux store] store to get headlines dispatched by actions"

---
+++ src/components/mainpanel/headlinePanel.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './headlinePanel.scss';
+export default class Side extends React.Component {
+    render() {
+        return (
+            <div className="maindiv">
+                <div className="source">
+                </div>
+                <div className="headlines">
+                </div>
+                <div className="navigation">
+                </div>
+            </div>
+        );
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const PORT = process.env.PORT || 8080;
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
