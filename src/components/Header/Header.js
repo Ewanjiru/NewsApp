9bd94ce37ca68503dc91ee440132d9f9dfa33a3f
Eunice Wanjiru <eunice.wanjiru@andela.com>
2017-05-26 12:34:05 +0300

    add logo image

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,6 @@ export default class Header extends React.Component {
 			<div className="header">
 				<div className="title"><h2>E-NEWZ</h2></div>
 				<div className="logo">
-					<img src="./../../../images/news_burned.png" alt="icon" />
 				</div>
 				<div className="loginIcon">
 					<Login />
