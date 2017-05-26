1c9e70943da2332538460a0f52dd9cfce1887340
Eunice Wanjiru <eunice.wanjiru@andela.com>
2017-05-26 09:50:01 +0300

    re add the files header,sidebar,footer

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './Header.scss';
+import Login from '../Login/Login.js';
+
+export default class Header extends React.Component {
+	render() {
+		return (
+			<div className="header">
+				<div className="title"><h2>E-NEWZ</h2></div>
+				<div className="logo">
+					<img src="./../../../images/news_burned.png" alt="icon" />
+				</div>
+				<div className="loginIcon">
+					<Login />
+				</div>
+			</div>
+		);
+	}
+}
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import newsStore from './../../stores/NewsStore';
+import NewsActions from './../../actions/NewsActions';
+import Sort from '../Sort/Sort';
+import './Sidebar.scss';
+
+export default class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+    const updateSelectedSource = this.props.updateSelectedSource;
+    this.state = {
+      sources: newsStore.getSources(),
+      selectedSource: newsStore.getSelectedSource(),
+      searchSource: newsStore.getSearchSource(),
+    };
+    this._onChange = this._onChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  componentDidMount() {
+    newsStore.addListener(this._onChange);
+    NewsActions.returnSources();
+  }
+
+  componentWillUnmount() {
+    newsStore.removeListener(this._onChange);
+  }
+
+  handleSearch(event) {
+    const searchInput = event.target.value;
+    this.setState({
+      searchSource: searchInput,
+    });
+    NewsActions.searchSource(searchInput);
+  }
+
+  _onChange() {
+    this.setState({
+      sources: newsStore.getSources(),
+      searchSource: newsStore.getSearchSource(),
+    });
+  }
+
+  render() {
+    let filteredSources;
+    const { searchSource } = this.state;
+    if (searchSource == '') {
+      filteredSources = this.state.sources;
+    } else {
+      filteredSources = this.state.sources.filter(sources => sources.name.toLowerCase().indexOf(this.state.searchSource.toLowerCase()) !== -1);
+    }
+    return (
+      <div className="aside">
+        <h5 className="search">Search A source</h5>
+        <input type="text" className="searchField" placeholder="search a source" onKeyUp={this.handleSearch} />
+        <Sort />
+        <div className="navbar">
+          {
+            filteredSources.map(newsSource => (
+              <ul
+                key={newsSource.id} value={newsSource.id} onClick={() => {
+                  this.props.updateSelectedSource(newsSource);
+                }}
+              >{newsSource.name}</ul>
+            ))
+          };
+        </div>
+      </div >
+    );
+  }
+}
